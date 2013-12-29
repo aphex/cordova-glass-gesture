2baf4d6e5e9e91433cbf24cb9f2f985f65cad1ac
Ross Gerbasi <rgerbasi@gmail.com>
2013-12-29 12:09:05 -0600

    Initial Commit adding gestures for
    - tap
    - longpress
    - swipeup
    - swipedown
    - swipeleft
    - swiperight
    - twotap
    - twolongpress
    - twoswipeup
    - twoswipedown
    - twoswipeleft
    - twoswiperight
    - threetap
    - threelongpress
    - scroll
    - twofingerscroll
    - fingercountchanged

---
+++ README.md
@@ -0,0 +1,77 @@
+##CordovaGlassGesture##
+---
+
+This plugin adds gesture support from the Google Glass Touchpad to your Cordova based Application.
+
+
+###Usage###
+
+Add plugin via Node
+
+`cordova plugin add https://github.com/aphex/cordova-glass-gesture`
+
+Add an event listener to the document of your web application
+
+```
+document.addEventListener('swiperight', 
+	function() {
+		console.log('Got that Swipe Right');
+	}
+);
+```
+
+###Basic Events###
+- tap
+- longpress
+- swipeup
+- swipedown
+- swipeleft 
+- swiperight
+- twotap
+- twolongpress
+- twoswipeup
+- twoswipedown
+- twoswipeleft
+- twoswiperight
+- threetap
+- threelongpress
+
+
+*A helpful note here a swipe to the left is backwards on the glass touchpad (from your eye towards your ear) where a swipe to the right is from your ear towards your eye.*
+
+###Advanced Events###
+Advanced events have data associated with them
+
+- scroll
+- twofingerscroll
+- fingercountchanged
+
+
+###Advanced Examples###
+
+```
+// Data contains properties displacement, delta and velocity
+document.addEventListener("scroll", function(data) {
+	console.log("Got that scroll");
+	console.log(data.displacement);
+	console.log(data.delta);
+	console.log(data.velocity);
+})
+```
+
+```
+// Data contains properties displacement, delta and velocity
+document.addEventListener("twofingerscroll", function(data) {
+	console.log("Got that two finger scroll");
+	console.log(data.displacement);
+	console.log(data.delta);
+	console.log(data.velocity);
+})
+```
+
+```
+// Data contains properties from and to
+document.addEventListener("fingercountchanged", function(data) {
+	console.log("Fingers changed from " + data.from + " to " + data.to);
+})
+```
---
+++ src/com/rossgerbasi/cordova/glass/GlassGesturePlugin.java
@@ -0,0 +1,91 @@
+package com.rossgerbasi.cordova.glass;
+
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class GlassGesturePlugin extends CordovaPlugin implements GestureDetector.BaseListener, GestureDetector.FingerListener, GestureDetector.ScrollListener, GestureDetector.TwoFingerScrollListener, View.OnGenericMotionListener {
+    private GestureDetector mGestureDetector;
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+//        Log.d("GlassGesturePlugin", "Glass Gesture Initialize");
+        super.initialize(cordova, webView);
+        this.webView.setOnGenericMotionListener(this);
+        mGestureDetector = new GestureDetector(this.webView.getContext());
+        mGestureDetector.setBaseListener(this);
+        mGestureDetector.setFingerListener(this);
+        mGestureDetector.setScrollListener(this);
+        mGestureDetector.setTwoFingerScrollListener(this);
+    }
+
+    @Override
+    public boolean onGenericMotion(View v, MotionEvent event) {
+        mGestureDetector.onMotionEvent(event);
+        return true;
+    }
+
+    @Override
+    public boolean onGesture(Gesture gesture) {
+        String type = gesture.toString().toLowerCase().replace("_","");
+        this.fireEvent(type, null);
+//        Log.d("GlassGesturePlugin", type);
+        return false;
+    }
+
+    @Override
+    public void onFingerCountChanged(int i, int i2) {
+        JSONObject data = new JSONObject();
+        try {
+            data.put("from", i);
+            data.put("to", i2);
+        } catch (JSONException e) {
+            Log.d("GlassGesturePlugin", "Exception setting FingerCountChanged data");
+        }
+
+        this.fireEvent("fingercountchanged", data);
+    }
+
+    @Override
+    public boolean onScroll(float v, float v2, float v3) {
+        JSONObject data = new JSONObject();
+        try {
+            data.put("displacement", v);
+            data.put("delta", v2);
+            data.put("velocity", v3);
+        } catch (JSONException e) {
+            Log.d("GlassGesturePlugin", "Exception setting inScroll data");
+        }
+
+        this.fireEvent("scroll", data);
+        return false;
+    }
+
+    @Override
+    public boolean onTwoFingerScroll(float v, float v2, float v3) {
+        JSONObject data = new JSONObject();
+        try {
+            data.put("displacement", v);
+            data.put("delta", v2);
+            data.put("velocity", v3);
+        } catch (JSONException e) {
+            Log.d("GlassGesturePlugin", "Exception setting onTwoFingerScroll data");
+        }
+
+        this.fireEvent("twofingerscroll", data);
+        return false;
+    }
+
+    private void fireEvent(String type, JSONObject data) {
+        String js = "javascript:try{cordova.fireDocumentEvent('"+type+"'" + (data != null  ? "," + data : "") +" );}catch(e){console.log('exception firing gesture event from native');};";
+        webView.loadUrl(js);
+//        Log.d("GlassGesturePlugin", "Sent: " + type + " with: " + data);
+    }
+}
